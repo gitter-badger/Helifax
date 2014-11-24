368ace78004f84ee1c89bf5ebfba1ac940ceb662
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-11-24 19:32:25 +0100

    Update results.php

--- application/views/results.php
+++ application/views/results.php
@@ -3,6 +3,14 @@
   
     <div class="col-xs-12 col-md-12 col-lg-12 col-sm-12">
       <h3> Resultaten </h4>
+      
+      <ul class="list unstyled">
+        <?php if($CreateDB == TRUE): ?>
+          <li><span class="octicon octicon-issue-closed"></span> Database created.</li>
+        <?php elseif($CreatDB == FALSE): ?>
+          <li><span class="octicon octicon-issue-opened"></span> Database not created.</li>
+        <?php endif; ?>
+      <ul>
     </div>
     
   </div>
