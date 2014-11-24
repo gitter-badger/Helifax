dda9f46e876804859a7deb179d7bec68f005f001
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-11-24 19:44:45 +0100

    Update results.php

--- application/views/results.php
+++ application/views/results.php
@@ -12,20 +12,30 @@
         <?php endif; ?>
         
         <?php if($TBL_1 == TRUE): ?>
+          <li><span class="octicon octicon-issue-closed"></span> Table regimenten created. </li>
         <?php elseif($TBL_1 == FALSE): ?>
+          <li><span class="octicon octicon-issue-opened"></span> Table regimenten not created. </li>
         <?php endif; ?>
         
+        
         <?php if($TBL_2 == TRUE): ?>
+          <li><span class="octicon octicon-issue-closed"></span> Table gesneuvelde created. </li>
         <?php elseif($TBL_2 == FALSE): ?>
+          <li><span class="octicon octicon-issue-openend"></span> Table gesneuvelde not created. </li>
         <?php endif; ?>
         
         <?php if($TBL_3 == TRUE): ?>
+          <li><span class="octicon octicon-issue-closed"></span> Table burgerslachtoffers created. </li>
         <?php elseif($TBL_3 == FALSE): ?>
+          <li><span class="octicon octicon-issue-openend"></span> Table burgerslachtoffers not cread</li>
         <?php endif; ?>
         
         <?php if($TBL_4 == TRUE): ?>
+          <li><span class="octicon octicon-issue-closed"></span> Table begraafplaatsen created.</li>
         <?php elseif($TBL_4 == FALSE): ?>
+          <li><span class="octicon octicon-issue-opened"></span> Table begraafplaatsen not created.</li>
         <?php endif; ?>
+        
       <ul>
     </div>
     
