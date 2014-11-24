3d56eb35f8d71b681e27babbea6725a3c3afebc8
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-11-24 20:00:16 +0100

    Update Setup.php

--- application/models/Setup.php
+++ application/models/Setup.php
@@ -3,9 +3,28 @@
     function CreateDB() {
       $this->dbforge->create_database('FallenSoldiers')
     }
-    
+
     function TBL_Regimenten() {
+      $fields = array(
+        'ID' => array(
+          'type'           =>
+          'constraint'     =>
+          'auto_increment' =>
+          ),
+          
+        'Regiment' => array(
+          'type'       =>
+          'constraint' =>
+          'default'    =>
+          ), 
+          
+        'Added' => array(),
+         'type'       => 'varchar'
+         'constraint' => '50'
+         'default'    => 'Onbekend',
+      );
       
+      // Build and send to server
     }
     
     function TBL_Gesneuvelde() {
