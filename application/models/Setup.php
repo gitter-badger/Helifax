ec1a9fc757ca1400e2eab29a09c07b3094ed7556
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-11-24 20:36:20 +0100

    Update Setup.php

--- application/models/Setup.php
+++ application/models/Setup.php
@@ -30,9 +30,30 @@
       $this->dbforge->add_key('ID', TRUE);
       $this->dbforge->create_table('Regimenten');
     }
-    
+ 
     function TBL_Gesneuvelde() {
+      $this->dbforge->add_field("PID int(11) unsigned NOT NULL AUTO_INCREMENT");
+      $this->dbforge->add_field("Voornaam VARCHAR(255) DEFAULT 'Niet gekend'");
+      $this->dbforge->add_field("Achternaam VARCHAR(45) DEFAULT 'Niet gekend'");
+      $this->dbforge->add_field("Burgerlijke_stand VARCHAR(255) DEFAULT NULL");
+      $this->dbforge->add_field("Stam_nr VARCHAR(30) DEFAULT NULL");
+      $this->dbforge->add_field('RegimentID INT(11) DEFAULT NULL');
+      $this->dbforge->add_field('Geboren_datum VARCHAR(45) DEFAULT NULL');
+      $this->dbforge->add_field('Geboren_plaats VARCHAR(200) DEFAULT NULL');
+      $this->dbforge->add_field('Overleden_datum VARCHAR(45) DEFAULT NULL');
+      $this->dbforge->add_field('Overleden_plaats VARCHAR(400) DEFAULT NULL');
+      $this->dbforge->add_field('Doodsoorzaak TEXT');
+      $this->dbforge->add_field('HerdenkingID INT(11) DEFAULT NULL');
+      $this->dbforge->add_field('Added VARCHAR(70) DEFAULT NULL');
+      $this->dbforge->add_field('Geslacht VARCHAR(45) DEFAULT NULL');
+      $this->dbforge->add_field("Eenheid VARCHAR(255) DEFAULT 'Niet gekend'");
+      $this->dbforge->add_field("Rang VARCHAR(45) DEFAULT 'Onbekend'");
+      $this->dbforge->add_field("Graf_referentie VARCHAR(45) DEFAULT 'Onbekend'");
+      $this->dbforge->add_field("Dienst VARCHAR(45) DEFAULT NULL");
       
+      // Build and send to server
+      $this->dbforge->add_key('PID', TRUE);
+      $this->dbforge->create_table('Regimenten');
     }
     
     function TBL_Burgerslachtoffers() {
