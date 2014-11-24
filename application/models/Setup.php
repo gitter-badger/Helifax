1f55c8b53afa3a53a9fccc452cc4535bbca7220c
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-11-24 19:21:03 +0100

    Update Setup.php

--- application/models/Setup.php
+++ application/models/Setup.php
@@ -1,3 +1,22 @@
 <?php 
   Class Setup extends CI_Model() {
+    function CreateDB() {
+      
+    }
+    
+    function TBL_Regimenten() {
+      
+    }
+    
+    function TBL_Gesneuvelde() {
+      
+    }
+    
+    function TBL_Burgerslachtoffers() {
+      
+    }
+    
+    function TBL_Begraafplaatsen() {
+      
+    }
   }
